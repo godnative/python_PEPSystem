723aaf35adcefc04d1e9db2b508184d5e8040f9e
冷月寒阳 <978953444@qq.com>
2025-07-13 00:10:49 +0800

    [sys]v1.1发布

--- utils/msyscfg.py
+++ utils/msyscfg.py
@@ -1,7 +1,7 @@
 import os
 
 AUTHOR = "堂区管理软件"
-VERSION = "1.0"
+VERSION = "1.1"
 
 SYSTEM_DATABASE_FILE_PATH = os.path.abspath("./DataBase/data.db").replace("\\", "/")
 SYSTEM_DATABASE_BACKUP_FILE_PATH = os.path.abspath("./DataBase/backup/").replace("\\", "/")
