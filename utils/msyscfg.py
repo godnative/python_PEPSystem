ed678faaa3f84eb4a4f1d52ac72069878f753fcc
yython <978953444@qq.com>
2025-12-07 22:04:45 +0800

    [SYS]update to V1.4

--- utils/msyscfg.py
+++ utils/msyscfg.py
@@ -3,7 +3,7 @@ import os
 CUR_SYS_TYPE = 0  # 0 为服务端 1 为客户端无本地存储
 if CUR_SYS_TYPE == 0:
     AUTHOR = "堂区管理软件"
-    VERSION = "1.3"
+    VERSION = "1.4"
 
     SYSTEM_DATABASE_FILE_PATH = os.path.abspath("./DataBase/data.db").replace("\\", "/")
     SYSTEM_DATABASE_BACKUP_FILE_PATH = os.path.abspath("./DataBase/backup/").replace("\\", "/")
